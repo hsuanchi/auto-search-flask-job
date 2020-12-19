dce10b0bc8abbd06e93bb372fb7bca2c06d8f32f
hsuanchi <a0025071@gmail.com>
2020-12-19 14:02:04 +0800

    feat(env): add poetry & requirements.txt

--- main.py
+++ main.py
@@ -2,58 +2,61 @@ import re
 import requests
 import pandas as pd
 
-from pprint import pprint
 from bs4 import BeautifulSoup
+
 from config import configs
 
 
-class Craw:
-    def __init__(self):
+class Crawer:
+    def __init__(self, keyword):
         self.data_list = []
+        self.keyword = keyword
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
         }
 
-    def fetch_data(self, config, keyword):
-        break_flag = False
-        page = 1
+    def fetch_data(self):
+        for config in configs:
 
-        while True:
-            if break_flag:
-                break
+            break_flag = False
+            page = 1
 
-            resp = requests.get(
-                url=config["platform_url"].format(keyword, page), headers=self.headers
-            )
-            soup = BeautifulSoup(resp.text, "html.parser")
-            job_list = eval(config["jobs_list_element"])
-
-            for job in job_list:
-                if not re.match(f".*{keyword}.*", str(job), re.IGNORECASE):
-                    break_flag = True
+            while True:
+                if break_flag:
                     break
 
-                company_name = eval(config["company_name_element"]).replace("|", "/")
-                company_list = eval(config["company_link_element"])
-                job_name = eval(config["job_name_element"]).replace("|", "/")
-                job_link = eval(config["job_link_element"])
-                job_dict = {
-                    "company_name": f"[{company_name}]({company_list})",
-                    "platform_name": config["platform_name"],
-                    "job_name": f"[{job_name}]({job_link})",
-                    "job_update_time": eval(config["job_update_time_element"]),
-                }
+                resp = requests.get(
+                    url=config["platform_url"].format(self.keyword, page), headers=self.headers
+                )
+                soup = BeautifulSoup(resp.text, "html.parser")
+                job_list = eval(config["jobs_list_element"])
 
-                self.data_list.append(job_dict)
-            page += 1
+                for job in job_list:
+                    if not re.match(f".*{self.keyword}.*", str(job), re.IGNORECASE):
+                        break_flag = True
+                        break
 
-    def mkd_to_readme(self, data_list):
+                    company_name = eval(config["company_name_element"]).replace("|", "/")
+                    company_list = eval(config["company_link_element"])
+                    job_name = eval(config["job_name_element"]).replace("|", "/")
+                    job_link = eval(config["job_link_element"])
+                    job_dict = {
+                        "company_name": f"[{company_name}]({company_list})",
+                        "platform_name": config["platform_name"],
+                        "job_name": f"[{job_name}]({job_link})",
+                        "job_update_time": eval(config["job_update_time_element"]),
+                    }
+
+                    self.data_list.append(job_dict)
+                page += 1
+
+    def mkd_to_readme(self):
         df = (
-            pd.DataFrame(data_list)
+            pd.DataFrame(self.data_list)
             .sort_values(["company_name", "platform_name"])
             .reset_index(drop=True)
         )
-        mkd = '## Auto searching job include Flask in Taiwan'
+        mkd = f'## Auto searching job include {self.keyword} in Taiwan'
         mkd += '\n\n\n'+df.to_markdown()
 
         with open("README.md", "w+") as f:
@@ -64,7 +67,6 @@ if __name__ == "__main__":
 
     keyword = "flask"
 
-    run = Craw()
-    for config in configs:
-        run.fetch_data(config, keyword)
-    run.mkd_to_readme(run.data_list)
+    run = Crawer(keyword)
+    run.fetch_data()
+    run.mkd_to_readme()
