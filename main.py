6fb688c27b735122cc0e3f9de6da5187ffeed29b
johnnyliu <bakolama@gmail.com>
2020-12-24 23:39:35 +0800

    feat: search multiple keywords by CLI
    e.g. `python main.py django flask`

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List
 
 import pandas as pd
@@ -5,13 +6,19 @@ import pandas as pd
 from crawlers import JOB_BANK_LIST
 
 class JobCrawler:
-    def __init__(self, keyword):
-        self.keyword = keyword
+    def __init__(self, keywords):
+        self.keywords = keywords
 
     def _create_new_readme(self):
-        mkd = f"# Auto searching job include {self.keyword} in Taiwan"
+        mkd = f"# Auto searching jobs"
 
         with open("README.md", "w+") as f:
+            f.write(mkd)
+
+    def _create_keyword_title(self, keyword):
+        mkd = f"\n----\n## Jobs include `{keyword}` in Taiwan"
+
+        with open("README.md", "a") as f:
             f.write(mkd)
 
     def _insert_to_readme(self, data_list: List[dict], platform_name: str):
@@ -29,14 +36,21 @@ class JobCrawler:
 
     def run(self):
         self._create_new_readme()
+        for keyword in self.keywords:
+            self._create_keyword_title(keyword)
 
-        for JobBank in JOB_BANK_LIST:
-            job_bank = JobBank(keyword)
-            job_bank.fetch_data()
-            self._insert_to_readme(job_bank.data_list, job_bank.platform_name)
+            for JobBank in JOB_BANK_LIST:
+                job_bank = JobBank(keyword)
+                job_bank.fetch_data()
+                self._insert_to_readme(job_bank.data_list, job_bank.platform_name)
 
 
 if __name__ == "__main__":
-    keyword = "flask"
-    crawler = JobCrawler(keyword)
+    if len(sys.argv) > 1:
+        keywords = sys.argv[1:]
+    else:
+        keywords = ["flask"]
+
+    print(f"You search keywords: {keywords}")
+    crawler = JobCrawler(keywords)
     crawler.run()
